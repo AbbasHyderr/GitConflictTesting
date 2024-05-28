06241b344585d096e86d3f605498f974b7d5836c
unknown <abbashyder2001@gmail.com>
2024-05-28 13:08:01 +0500

    Commit from Abbas

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using RepoTest.RepoTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestCLass test = new TestCLass();
+            test.Caller();
+            Console.ReadKey();
+                
+        }
+    }
+}
---
+++ RepoTest/TestCLass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoTest.RepoTest
+{
+    public class TestCLass
+    {
+        private int _id = 1;
+        public void Caller()
+        {
+            _id++;
+            Console.WriteLine(_id);
+        }
+    }
+}
